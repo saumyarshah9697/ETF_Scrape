73c1b9ca2082055fc756e8b077a1c76f855490e6
Harish Ramani <linkinnation1792@gmail.com>
2019-06-14 11:46:36 -0400

    Add files via upload
    Scrape etf holdings

---
+++ scrape_holdings.py
@@ -0,0 +1,23 @@
+import requests
+import json
+info = []
+def parse_response(data, info):
+    for row in data["rows"]:
+        name = row["holding"].split('>')[1].split('(')[0]
+        print(name)
+        holding = {
+            "name": name,
+            "weight": row["weight"]
+        }
+        info.append(holding)
+    return info
+
+for i in range(1,2):
+    req_url ='https://etfdb.com/data_set/?tm=40284&cond={%22by_etf%22:783}&no_null_sort=true&count_by_id=&sort=weight&order=desc&limit='+str(15*i)+'&offset='+str(15*(i-1))
+    print(req_url)
+    resp = requests.get(req_url)
+    data = resp.content
+    jsn = json.loads(data)
+    info = parse_response(jsn, info)
+
+print("Info is", info)
